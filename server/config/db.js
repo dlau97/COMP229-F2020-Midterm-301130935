0ce5533bb6325767ce085a873f176a8fb045774f
Dhevan Lau <dhevan999@gmail.com>
2021-03-06 12:58:05 +0200

    Mid Project Commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    "URI":"mongodb://localhost/book-store"
+    //"URI":"mongodb+srv://dhevan:Toolaud572@mongodbserver.oicwq.mongodb.net/business_contacts?retryWrites=true&w=majority"
+}
---
+++ server/routes/book.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// connect to our Book Model
+let Book = require('../models/book');
+
+let bookController = require('../controllers/book');
+
+
+/* GET Route for the Book List page - READ Operation */
+router.get('/', bookController.displayBookList);
+
+/* GET Route for displaying the Add page - CREATE Operation */
+router.get('/add', bookController.displayAddPage);
+
+/* POST Route for processing the Add page - CREATE Operation */
+router.post('/add', bookController.processAddPage);
+
+/* GET Route for displaying the Edit page - UPDATE Operation */
+router.get('/edit/:id', bookController.displayEditPage);
+
+// Post Route for processing the Edit list page  - Update Operation
+router.post('/edit/:id', bookController.processEditPage);
+
+// Get to perform Deletion  - Deletion Operation
+router.get('/delete/:id', bookController.performDelete);
+
+module.exports = router;
